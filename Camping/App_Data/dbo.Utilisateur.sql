1b5d080bc1cad1ec5a416ec0b95e10c22fb52c35
alexanderdelforge <delforgealexander@gmail.com>
2015-09-23 15:16:01 +0200

    Base de données + création de la table utilisateur

---
+++ Camping/App_Data/dbo.Utilisateur.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Utilisateur]
+(
+	[id_user] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [nom_user] VARCHAR(100) NOT NULL, 
+    [prenom_user] VARCHAR(100) NOT NULL, 
+    [mail_user] VARCHAR(100) NOT NULL, 
+    [motdepasse_user] VARCHAR(100) NOT NULL
+)
