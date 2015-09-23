c0b3b01b93bc92c7beb505b7e28bb847c0cfaf2d
alexanderdelforge <delforgealexander@gmail.com>
2015-09-23 23:26:26 +0200

    Base de données

--- Camping/Controllers/HomeController.cs
+++ Camping/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Camping.Models;
 
 namespace Camping.Controllers
 {
@@ -11,20 +12,88 @@ namespace Camping.Controllers
     {
         private object id;
 
+        
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var entities = new CampingEntities();
+            return View(entities.Utilisateur.ToList());
         }
+
+        
 
         public ActionResult Connexion()
         {
             return View();
         }
 
-        public ActionResult Inscription()
+        public ActionResult Inscription(Utilisateur user)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+            return View();
+        }
+
+        // GET: /Home/Details/5
+
+        public ActionResult Details(int id)
         {
             return View();
+        }
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Edit/5
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
