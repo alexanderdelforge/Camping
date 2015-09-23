60f6af876874cf080bba670aab50c15a372cc709
alexanderdelforge <delforgealexander@gmail.com>
2015-09-23 13:45:53 +0200

    Test

---
+++ Camping/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Camping.Controllers
+{
+    public class HomeController : Controller
+    {
+        private object id;
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Connexion()
+        {
+            return View();
+        }
+
+        public ActionResult Inscription()
+        {
+            return View();
+        }
+    }
+}
